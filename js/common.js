55d391ea80a3b942c3ae8c48ad4791eb38605845
JEISOO <153473478+JEISOO@users.noreply.github.com>
2024-04-24 12:06:23 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,6 +1,25 @@
-setTimeout(function() {
+setTimeout(function(){
     $("body").addClass('on')
 }, 10)
+$("#main p").delay(1500).animate({opacity: 1});
+$("#main h2").delay(2000).animate({opacity: 1});
+
+
+
+$(document).ready(function(){
+
+    const img = $(".inimg").height();
+
+    $(window).scroll(function(){ 
+    const rollIt = $(this).scrollTop() >= img; 
+        
+
+    if(rollIt ){ 
+            $("#introduction .inimg").show().css({opacity: 1});
+        }
+        else{
+            $("#introduction .inimg").hide();
+        }
+    });
     
-$("#main p").delay(1500).animate({ opacity: 1 })
-$("#main h2").delay(2000).animate({ opacity: 1 })
+});
