5ef36d002c8353ef0059e78f6726a0f89ec47acf
JEISOO <153473478+JEISOO@users.noreply.github.com>
2024-04-26 17:06:09 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -5,15 +5,16 @@
 $("#main p").delay(50).animate({opacity: 1}, 1000);
 $("#main h2").delay(1000).animate({opacity: 1}, 1000);
 
-// introduction 이미지 나타날때
 
-const img = $(".inimg").offset().top- 250;
+// 각 섹션 도달시 
+const img = $(".inimg").offset().top- 350;
 console.log(img)
 $(window).scroll(function(){ 
     if ( $(window).scrollTop() >= img) { 
         $("#introduction .inimg").animate({opacity: 1}, 1000);
     }
 });
+    
 
 
 
